590e869e6e34605566a53c9fd651969f028f3e0f
sohammehta95 <32079958+sohammehta95@users.noreply.github.com>
2018-07-11 19:06:01 -0700

    ENV DATABASE

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect("mongodb://soham:soham1@ds233551.mlab.com:33551/bucketz-db", { useMongoClient: true });
+mongoose.connect(process.env.DATABASEURL, { useMongoClient: true });
 
 mongoose.Promise = Promise;
 
